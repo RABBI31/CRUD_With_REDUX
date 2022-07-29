a7da750fd28c07ee1ddbb6d2594cf12230f737ff
Md.Ali Rabbi <56753742+RABBI31@users.noreply.github.com>
2022-07-29 23:59:15 +0600

    Folder structure

---
+++ src/features/Books/ShowBooks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SHowBooks = () => {
+  return (
+    <div>SHowBooks</div>
+  )
+}
+
+export default SHowBooks
---
+++ src/routes/Index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+const Index = () => {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default Index
